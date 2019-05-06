69f52a9b4d18ed827838423f736e863f0bbeb267
annettesunday <annette.odhiambo@andela.com>
2019-05-06 12:33:18 +0300

    ch(app): create home, about, courses, pageNotFound pages, and  modify layout

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => (
+    <div>
+        <h1>About</h1>
+        <p>This application uses React, Redux and a variety of helpful libraries</p>
+    </div>
+);
+
+
+export default AboutPage;
